787e92e2b7f6e039ff39ada3da7a086a97caba93
smilingpigs <ajaiprakaz@gmail.com>
2023-11-15 22:41:33 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/hospitaly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hospitaly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(widget.title),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.teal,
+        child: const Text("hospitaly",
+        style: TextStyle(
+          fontSize: 90,
+          color: Colors.white,
+          fontWeight: FontWeight.w100,
+          fontStyle: FontStyle.italic,
+          fontFamily: 'Roboto'
+        ),),
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
